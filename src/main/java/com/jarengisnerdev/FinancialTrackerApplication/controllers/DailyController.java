96694740cda117c3694446eebe5499a587d7a911
jarengisner <124743127+jarengisner@users.noreply.github.com>
2024-05-03 09:25:36 -0600

    Finished refactoring daily controller for JWT and error handling

--- src/main/java/com/jarengisnerdev/FinancialTrackerApplication/controllers/DailyController.java
+++ src/main/java/com/jarengisnerdev/FinancialTrackerApplication/controllers/DailyController.java
@@ -2,6 +2,7 @@ package com.jarengisnerdev.FinancialTrackerApplication.controllers;
 
 import com.jarengisnerdev.FinancialTrackerApplication.interfaces.DailyService;
 import com.jarengisnerdev.FinancialTrackerApplication.models.Dailys;
+import com.jarengisnerdev.FinancialTrackerApplication.utility.JwtUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,62 +18,116 @@ public class DailyController {
     DailyService dailyService;
 
     @GetMapping("/daily/{id}")
-    public ResponseEntity<Dailys> getDailyById(@PathVariable Long id){
-        Dailys currentQueryDaily = dailyService.getDailyById(id);
+    public ResponseEntity<Dailys> getDailyById(@PathVariable Long id, @RequestHeader("Authorization") String token){
 
-        if(currentQueryDaily == null){
-           return ResponseEntity.notFound().build();
-        }else{
-            return ResponseEntity.ok(currentQueryDaily);
+        try{
+            if(token != null && token.startsWith("Bearer ")){
+                String jwtToken = token.substring(7);
+
+                if(!JwtUtil.validateToken(jwtToken)){
+                    return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+                }
+
+                Dailys currentQueryDaily = dailyService.getDailyById(id);
+
+                if(currentQueryDaily == null){
+                    return ResponseEntity.notFound().build();
+                }else{
+                    return ResponseEntity.ok(currentQueryDaily);
+                }
+
+            }else{
+                return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+            }
+        }catch(Exception e){
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
         }
     };
 
     @GetMapping("/daily/{trackerId}/all")
-    public ResponseEntity<List<Dailys>> getAllDailyByTrackerId(@PathVariable Long trackerId){
-        List<Dailys> currentQueryDailyList = dailyService.getAllDailyByTrackerId(trackerId);
+    public ResponseEntity<List<Dailys>> getAllDailyByTrackerId(@PathVariable Long trackerId, @RequestHeader("Authorization") String token){
+        try{
+            if(token != null && token.startsWith("Bearer ")){
+                String jwtToken = token.substring(7);
 
-        if(currentQueryDailyList.isEmpty()){
-            return ResponseEntity.noContent().build();
-        }else{
-            return ResponseEntity.ok(currentQueryDailyList);
+                if(!JwtUtil.validateToken(jwtToken)){
+                    return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+                }
+
+                List<Dailys> currentQueryDailyList = dailyService.getAllDailyByTrackerId(trackerId);
+
+                if(currentQueryDailyList.isEmpty()){
+                    return ResponseEntity.noContent().build();
+                }else{
+                    return ResponseEntity.ok(currentQueryDailyList);
+                }
+
+            }else{
+                return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+            }
+        }catch(Exception e){
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
         }
     };
 
     //date format must follow YYYY-MM-DD format
     @PostMapping("/daily")
-    public ResponseEntity<?> createNewDaily(@RequestBody Dailys daily){
+    public ResponseEntity<?> createNewDaily(@RequestBody Dailys daily, @RequestHeader("Authorization") String token){
         try{
-            Dailys brandNewDaily = dailyService.createNewDaily(daily);
+            if(token != null && token.startsWith("Bearer ")){
+                String jwtToken = token.substring(7);
 
-            return new ResponseEntity<>(brandNewDaily,HttpStatus.CREATED);
+                if(!JwtUtil.validateToken(jwtToken)){
+                    return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+                }
+
+                Dailys brandNewDaily = dailyService.createNewDaily(daily);
+
+                return new ResponseEntity<>(brandNewDaily,HttpStatus.CREATED);
+            }else{
+                return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+            }
         }catch(Exception e){
             return new ResponseEntity<>("Failed to create new Daily" + e.getMessage(),HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 
     @PutMapping("/daily/update/{id}")
-    public ResponseEntity<?> updateDailyById(@PathVariable Long id, @RequestBody Dailys daily){
-        Dailys currentDailyToEdit = dailyService.getDailyById(id);
+    public ResponseEntity<?> updateDailyById(@PathVariable Long id, @RequestBody Dailys daily, @RequestHeader("Authorization") String token){
+        try{
+            if(token != null && token.startsWith("Bearer ")){
+                String jwtToken = token.substring(7);
 
-        if(currentDailyToEdit == null){
-            return ResponseEntity.notFound().build();
-        }else {
-            currentDailyToEdit.setSaved(daily.getSaved());
-            currentDailyToEdit.setSavedNote(daily.getSavedNote());
-            currentDailyToEdit.setNeeds(daily.getNeeds());
-            currentDailyToEdit.setNeedsNote(daily.getNeedsNote());
-            currentDailyToEdit.setWants(daily.getWants());
-            currentDailyToEdit.setWantsNote(daily.getWantsNote());
+                if(!JwtUtil.validateToken(jwtToken)){
+                    return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+                }
 
-            //Attempt to save the new record after updating, with error handling if needed
-            try{
-                Dailys dailyAfterEditing = dailyService.updateDaily(currentDailyToEdit);
-                return new ResponseEntity<>(dailyAfterEditing, HttpStatus.CREATED);
-            }catch (Exception e){
-                return new ResponseEntity<>("Failed to update daily record: " + e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+                Dailys currentDailyToEdit = dailyService.getDailyById(id);
+
+                if(currentDailyToEdit == null){
+                    return ResponseEntity.notFound().build();
+                }else {
+                    currentDailyToEdit.setSaved(daily.getSaved());
+                    currentDailyToEdit.setSavedNote(daily.getSavedNote());
+                    currentDailyToEdit.setNeeds(daily.getNeeds());
+                    currentDailyToEdit.setNeedsNote(daily.getNeedsNote());
+                    currentDailyToEdit.setWants(daily.getWants());
+                    currentDailyToEdit.setWantsNote(daily.getWantsNote());
+
+                    Dailys dailyAfterEditing = dailyService.updateDaily(currentDailyToEdit);
+                    return new ResponseEntity<>(dailyAfterEditing, HttpStatus.CREATED);
+                }
+
+            }else{
+                return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
             }
-
+        }catch(Exception e){
+            return new ResponseEntity<>("Failed to update daily record: " + e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
         }
+
+
+
+
     };
 
 
